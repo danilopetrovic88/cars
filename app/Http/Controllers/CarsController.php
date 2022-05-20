f9a4a3633e09e1512b6a75d18a3b5a866d9d9a52
danilopetrovic88 <vivify@VivifyIdeass-MacBook-Pro-3.local>
2022-05-20 19:14:52 +0200

    napravljena show metoda i view za single car

--- app/Http/Controllers/CarsController.php
+++ app/Http/Controllers/CarsController.php
@@ -13,4 +13,11 @@ class CarsController extends Controller
 
         return view('cars', compact('cars'));
     }
+
+    public function show($id)
+    {
+        $car = Car::find($id);
+
+        return view('car', compact('car'));
+    }
 }
